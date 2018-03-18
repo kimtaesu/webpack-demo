c31fa399ec4b067a2baa735db661bdb71eb6c39c
kimtaesu <kimtaesoo188@gmail.com>
2018-03-18 11:14:49 +0900

    Source Mapping

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,8 +2,12 @@ const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
 
+// Avoid inline-*** and eval-*** use in production as they can increase bundle size and reduce the overall performance.
 module.exports = merge(common, {
+    devtool: 'source-map',
     plugins: [
-        new UglifyJSPlugin()
+        new UglifyJSPlugin({
+            sourceMap: true
+        })
     ]
 });
