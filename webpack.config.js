c9556578406f2687ebef5da9c1dca261ff3b330a
kimtaesu <kimtaesoo188@gmail.com>
2018-03-18 11:41:28 +0900

    Dynamic Imports 동작이 잘 안됨..

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ const webpack = require('webpack');
 module.exports = {
     entry: {
         index: './src/index.js',
-        another: './src/another-module.js'
     },
     plugins: [
         new HTMLWebpackPlugin({
@@ -15,13 +14,7 @@ module.exports = {
     ],
     output: {
         filename: '[name].bundle.js',
+        chunkFilename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
-    // CommonsChunkPlugin이 웹팩4에서 optimize 속성의 splitChunks로 이전되었습니다.
-    // 아래의 예시는 react의 경우에 대부분의 청크가 공통적으로 사용하는 모듈들을 vendor라는 entry 이름으로 추출하는 겁니다.
-    optimization: {
-        splitChunks: {
-            name: 'vendor'
-        }
-    }
 };
